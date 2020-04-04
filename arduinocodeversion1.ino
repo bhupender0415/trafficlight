58be953b3968d7f81595f6d613e7b03cbd52f561
bhupender0415 <63164983+bhupender0415@users.noreply.github.com>
2020-04-05 00:41:36 +0530

    Update arduinocodeversion1.ino

--- arduinocodeversion1.ino
+++ arduinocodeversion1.ino
@@ -1,13 +1,15 @@
-// variables
-int GREEN = 2;
-int YELLOW = 3;
-int RED = 4;
+// hi my name is bhupender and i am an electronic hobbyist 
+// this is a simple code for arduino to make a trffic light system 
+// define variables
+int GREEN = 2; // pin 2 of ardion as GREEN
+int YELLOW = 3; // pin 3 of ardion as YELLOW
+int RED = 4; // pin $ of ardion as RED
 int DELAY_GREEN = 15000;
 int DELAY_YELLOW = 5000;
 int DELAY_RED = 10000;
 
 
-// basic functions
+// In void setup we define our inputs/outputs
 void setup()
 {
   pinMode(GREEN, OUTPUT);
@@ -27,21 +29,21 @@ void loop()
 
 void green_light()
 {
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, HIGH); // green on 
+  digitalWrite(YELLOW, LOW);  // yellow off
+  digitalWrite(RED, LOW);  // red off
 }
 
 void yellow_light()
 {
-  digitalWrite(GREEN, LOW);
-  digitalWrite(YELLOW, HIGH);
-  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW); // green off
+  digitalWrite(YELLOW, HIGH);  // yellow on
+  digitalWrite(RED, LOW); // red off
 }
 
 void red_light()
-{
-  digitalWrite(GREEN, LOW);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, HIGH);
+{ 
+  digitalWrite(GREEN, LOW); // green off
+  digitalWrite(YELLOW, LOW);// yellow off
+  digitalWrite(RED, HIGH); // red on
 }
